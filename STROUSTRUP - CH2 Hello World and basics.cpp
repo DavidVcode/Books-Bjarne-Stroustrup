9b9e3149db9b90e8715966ffccd0cab5d452b083
DavidVcode <43756672+DavidVcode@users.noreply.github.com>
2019-01-27 18:57:31 -0500

    Add files via upload

---
+++ STROUSTRUP - CH2 Hello World and basics.cpp
@@ -0,0 +1,22 @@
+// STROUSTRUP - CH2 Hello World and basics.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// APPARENTLY GIVEN THE B:AN STATUS OF THIS PROJECT, MOST OF THE NOTES ARE IN "PROGRAMMING - STROUSTRUP PROGRAMMING".docx 
+//There probably wasn't much we didn't a;lready know about printing hello world except advanced hooliganary
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
